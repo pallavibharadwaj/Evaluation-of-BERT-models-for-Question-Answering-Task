42d8e310413505c4bcb61aa23d44f43a40814d7c
pallavib <pallavib@sfu.ca>
2020-11-30 09:51:32 +0530

    Comparing models Distilbert and Roberta

---
+++ source/run.sh
@@ -0,0 +1,13 @@
+rm -r cache_dir
+python train.py roberta
+
+rm -r cache_dir
+python train.py distilbert
+
+rm -r cache_dir
+python train.py electra-base
+
+'''
+rm -r cache_dir
+python train.py electra-small
+'''
