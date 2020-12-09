8cf2d571419b03727ac92025b39755d845d19534
Najeeb Qazi <najeebq@gmail.com>
2020-12-08 23:18:34 -0800

    Changes to project.ipynb

--- web_application/app.test.py
+++ web_application/app.test.py
@@ -3,7 +3,7 @@ from app import app
 class BasicTestCase(unittest.TestCase):
 def test_home(self):
         tester = app.test_client(self)
-        pages = ['/', 'about', 'register', 'login']
+        pages = ['/']
         for page in pages:
             response = tester.get(page, content_type='html/text')
             self.assertEqual(response.status_code, 200)
